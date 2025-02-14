9d3a52e791115369ca6ff8dc1bdb18274f35bc9a
RASERUI <russellroy.ignacio@lorma.edu>
2025-02-14 16:53:35 +0800

    Initialization

---
+++ Main.java
@@ -0,0 +1,40 @@
+class GradeTracker {
+    public static void main(String[] args) {
+        String studentName = "RUSSELL ROY M. IGNACIO";
+        String course = "BSIT-1";
+
+        String[][] subjects = {
+                {"CFVE-1"},
+                {"DISTRUC1"},
+                {"PURCOM"},
+                {"ART APP"},
+                {"STS"},
+                {"NSTP"},
+                {"PATHFIT"}
+        };
+
+        double[][] grades = {
+                {80.38, 80.97, 89.83},
+                {87.22, 82.84, 67.60},
+                {74.17, 81.27, 83.98},
+                {86.35, 80.83, 63.79},
+                {80.56, 72.78, 73.78},
+                {73.03, 74.24, 89.70},
+                {92.67, 84.41, 92.93}
+        };
+
+        System.out.println(studentName + " from " + course + "\n");
+
+        System.out.println("Grade Report:");
+        System.out.println(String.format("%-12s %-12s %-12s %-12s %-12s", "SUBJECT", "PRELIM", "MID TERM", "FINALS", "FINAL GRADE"));
+
+        for (int i = 0; i < subjects.length; i++) {
+            double finalGrade = calculateFinalGrade(grades[i]);
+            System.out.println(String.format("%-12s %-12.2f %-12.2f %-12.2f %-12.2f", subjects[i][0], grades[i][0], grades[i][1], grades[i][2], finalGrade));
+        }
+    }
+
+    public static double calculateFinalGrade(double[] grades) {
+        return grades[0] * 0.3 + grades[1] * 0.3 + grades[2] * 0.4;
+    }
+}
