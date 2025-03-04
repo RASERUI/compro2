e79bd6794ccde2ff40c8b2b9dbe584a71cf87deb
RASERUI <russellroy.ignacio@lorma.edu>
2025-03-04 23:04:35 +0800

    Add files via upload

---
+++ springbootact1/src/main/java/com/google/projectx/Coffee.java
@@ -0,0 +1,14 @@
+package com.google.projectx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Controller
+public class ProjectxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectxApplication.class, args);
+	}
+
+}
