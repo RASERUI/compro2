67678255943f92ef78e63ce4900565076c569e88
RASERUI <russellroy.ignacio@lorma.edu>
2025-03-05 08:53:53 +0800

    Initialize commit

---
+++ src/main/java/com/google/SBA1/Sba1Application.java
@@ -0,0 +1,13 @@
+package com.google.SBA1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sba1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sba1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/SBA1/Sba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.SBA1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sba1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
