f030efcffacd72ba89b9307d6ef4ed449d324487
RASERUI <russellroy.ignacio@lorma.edu>
2025-04-30 09:38:41 +0800

    Initailzie

---
+++ src/test/java/com/russellroy/a1/Validation/Form/ValidationFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.russellroy.a1.Validation.Form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
